8e47049d9ae05b69a2f5b27aef3d9e7209b36eaa
Taylan Tatlı <taylantatli90@gmail.com>
2023-02-11 00:58:14 +0300

    Add context menu for back and forth

--- main.js
+++ main.js
@@ -1,45 +1,69 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow, shell} = require('electron')
-const contextMenu = require('electron-context-menu');
-const path = require('path')
+const { app, BrowserWindow, shell } = require("electron");
+const contextMenu = require("electron-context-menu");
+const path = require("path");
 
-app.commandLine.appendSwitch('enable-tcp-fastopen')
-app.commandLine.appendSwitch('enable-features', 'VaapiVideoDecoder')
+app.commandLine.appendSwitch("enable-tcp-fastopen");
+app.commandLine.appendSwitch("enable-features", "VaapiVideoDecoder");
 
 contextMenu({
-	prepend: (defaultActions, params, browserWindow, dictionarySuggestions) => [
-		{
-			label: 'Search Google for “{selection}”',
-			// Only show it when right-clicking text
-			visible: params.selectionText.trim().length > 0,
-			click: () => {
-				shell.openExternal(`https://google.com/search?q=${encodeURIComponent(params.selectionText)}`);
-			}
-		}
-	]
+  prepend: (defaultActions, params, browserWindow, dictionarySuggestions) => [
+    {
+      label: "Search Google for “{selection}”",
+      // Only show it when right-clicking text
+      visible: params.selectionText.trim().length > 0,
+      click: () => {
+        shell.openExternal(
+          `https://google.com/search?q=${encodeURIComponent(
+            params.selectionText
+          )}`
+        );
+      },
+    },
+    {
+      label: "Back",
+      visible: !params.isEditable && !params.linkURL,
+      enabled: browserWindow.webContents.canGoBack(),
+      click: () => browserWindow.webContents.goBack(),
+    },
+    {
+      label: "Forward",
+      visible: !params.isEditable && !params.linkURL,
+      enabled: browserWindow.webContents.canGoForward(),
+      click: () => browserWindow.webContents.goForward(),
+    },
+    {
+      label: "Reload",
+      visible: !params.isEditable && !params.linkURL,
+      click: () => browserWindow.webContents.reload(),
+    },
+  ],
 });
 
-function createWindow () {
+function createWindow() {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
     width: 1280,
     height: 750,
-    icon: __dirname + '/icon.png',
-    backgroundColor: '#2C2C2C',
+    icon: __dirname + "/icon.png",
+    backgroundColor: "#2C2C2C",
     webPreferences: {
-     contextIsolation: true,
-     preload: path.join(app.getAppPath(), 'preload.js')
-    }
-  })
+      contextIsolation: true,
+      preload: path.join(app.getAppPath(), "preload.js"),
+    },
+  });
 
   // and load the index.html of the app.
-  mainWindow.loadFile('splash.html')
+  mainWindow.loadFile("splash.html");
   setTimeout(function () {
-    mainWindow.loadURL('https://www.youtube.com/',{ userAgent: "Mozilla/5.0 (YoutubeMusic-Desktop) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36'"});
-  }, 3000) // Load store page after 3 secs
-  mainWindow.maximize() // start maximized
-  mainWindow.setMenuBarVisibility(false)
-  mainWindow.setMenu(null)
+    mainWindow.loadURL("https://www.youtube.com/", {
+      userAgent:
+        "Mozilla/5.0 (YoutubeMusic-Desktop) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36'",
+    });
+  }, 3000); // Load store page after 3 secs
+  mainWindow.maximize(); // start maximized
+  mainWindow.setMenuBarVisibility(false);
+  mainWindow.setMenu(null);
   mainWindow.show();
 
   // Open the DevTools.
@@ -49,20 +73,20 @@ function createWindow () {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.whenReady().then(createWindow)
+app.whenReady().then(createWindow);
 
 // Quit when all windows are closed.
-app.on('window-all-closed', function () {
+app.on("window-all-closed", function () {
   // On macOS it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') app.quit()
-})
+  if (process.platform !== "darwin") app.quit();
+});
 
-app.on('activate', function () {
+app.on("activate", function () {
   // On macOS it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
-  if (BrowserWindow.getAllWindows().length === 0) createWindow()
-})
+  if (BrowserWindow.getAllWindows().length === 0) createWindow();
+});
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
